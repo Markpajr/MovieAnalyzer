96177873c5585cd7eb98a683bf563b55c7034e42
Markpajr <Shadowstar88@Hotmail.com>
2019-12-28 23:27:05 -0500

    Switch Movie scraper to rotten tomatos

--- MovieGrabber.py
+++ MovieGrabber.py
@@ -1,18 +1,32 @@
 import bs4, requests
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep
+
+def Scrape_Movies():
+    rt_movies_url = "https://www.rottentomatoes.com/browse/dvd-streaming-all?minTomato=0&maxTomato=100&services=amazon;hbo_go;itunes;netflix_iw;vudu;amazon_prime;fandango_now&genres=1;2;4;5;6;8;9;10;11;13;18;14&sortBy=release"
+    driver_path = r"C:\Python\chromedriver.exe" 
+    driver = webdriver.Chrome(driver_path)
+    driver.get(rt_movies_url)
+
+    # sleep(timeout)
+    # try:
+    #     element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[4]/c-wiz/div/c-wiz/div/c-wiz/c-wiz/c-wiz/div/div[2]/div[2]/c-wiz/div/div/div[2]/div/div/div[1]/div/div/div[1]/a/div'))
+    #     WebDriverWait(driver, timeout).until(element_present)
+    # except TimeoutException:
+    #     print ("Timed out waiting for page to load")
 
 
-driver_path = r"C:\Python\chromedriver.exe" 
-Chrome = webdriver.Chrome(driver_path)
-g_movies_url = "https://play.google.com/store/movies/collection/cluster?clp=6gIkIiIKHHByb21vdGlvbl9tb3ZpZXNfbmV3X3JlbGVhc2UQBxgE:S:ANO1ljIZSCM&gsr=CifqAiQiIgoccHJvbW90aW9uX21vdmllc19uZXdfcmVsZWFzZRAHGAQ%3D:S:ANO1ljKPEAQ&hl=en_US"
+    movies_soup = bs4.BeautifulSoup(driver.page_source, "lxml")
+
+    print(movies_soup.prettify())
 
 
 
-# res = requests.get(g_movies_url)
-# res.raise_for_status()
-
-
-movies_soup = bs4.BeautifulSoup(res.text)
-print(movies_soup.prettify())
-
-print(type(movies_soup))
+if __name__ == "__main__":
+    #Run Scraper
+    Scrape_Movies()
+    print("done")
